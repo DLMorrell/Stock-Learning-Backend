91a7f0955cfe5c8da7db162f7bdda0aa9081de5a
Brian Dearing <smartsnake1@gmail.com>
2020-10-05 19:16:21 -0500

    Added get all alpaca market data
    *Can now get all of a given stocks historical data.
    *Added a buying simulator for alpaca

--- Tools/get_alpaca_market_data.py
+++ Tools/get_alpaca_market_data.py
@@ -3,6 +3,7 @@ import os
 from time import sleep
 from dotenv import load_dotenv
 import pandas as pd
+from datetime import datetime
 
 load_dotenv()
 
@@ -16,8 +17,26 @@ class market_data():
     def getPrice(self, barDuration, symbol, limit ):
         price = self.alpaca.get_barset(symbol, barDuration, limit = limit)
         symbol_price = price[symbol]
-
         return symbol_price
+
+
+    def getPrice(self, barDuration, symbol, limit, endDate ):
+        price = self.alpaca.get_barset(symbol, barDuration, limit = limit, end = endDate)
+        symbol_price = price[symbol]
+        return symbol_price
+
+    def getAllData(self, stock):
+        startDate = "2015-01-04T00:00:00Z"#Beginning of time if possible
+        endDate = "2020-10-02T00:00:00Z"#Change to todays date
+        allData = []
+        while datetime.strptime(startDate, '%Y-%m-%dT%H:%M:%SZ') <= datetime.strptime(endDate, '%Y-%m-%dT%H:%M:%SZ'):
+            sampleData = self.getPrice('1Min', stock, 1000, endDate)
+            endDate = str(sampleData[0].t)[:len(str(sampleData[0].t))-6].replace(' ', 'T')+'Z'
+            for bar in sampleData:
+                allData.insert(0, f'{bar.t}, {bar.o}, {bar.h}, {bar.l}, {bar.c}, {bar.v}')
+
+        return allData
+    
     
     # Number of shares owned
     def getShares(self, symbol):
